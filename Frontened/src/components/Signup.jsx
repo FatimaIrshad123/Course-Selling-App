b4c47c270114f48cf1a721bcd7fa28f308e73bc2
FatimaIrshad <bibimemoona2017@gmail.com>
2024-01-26 00:03:37 +0500

    Added Login Page

---
+++ Frontened/src/components/Signup.jsx
@@ -0,0 +1,73 @@
+import { TextField, Typography } from "@mui/material";
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { useState } from "react";
+
+export default function Signup() {
+    const [username,setUsername] = useState('')
+    const [password,setpassword] = useState('')
+
+    return (
+      <div>
+        <center>
+          <div style={{paddingTop:150,paddingBottom: 25}}>
+            <Typography variant={"h4"}> Welcome </Typography>
+          </div>
+        </center>
+        
+        <center> 
+        <Card variant="outlined" style = {{border: '2px solid black',width:400,padding:20}}>
+          <TextField 
+                // id="standard-basic" 
+                label="Username" 
+                variant="standard" 
+                fullWidth={true} 
+                id = {'username'}
+                type="text" 
+                value={username}
+                onChange={e => setUsername(e.target.value)}/>
+          <br /><br />
+           <TextField 
+                // id="standard-basic" 
+                label="Password" 
+                variant="standard" 
+                id = {'password'}
+                fullWidth={true} 
+                type="password" 
+                value={password}
+              onChange={e => setpassword(e.target.value)}/>
+          <br /><br />
+          
+          <Button 
+              variant="contained" 
+              size={"larger"} 
+              onClick={() => {
+                
+                function callback2(data){
+                  localStorage.setItem('token',data.token)
+                  console.log(data);
+                }
+
+                function callback1(res){
+                  res.json().then(callback2)
+                }
+                fetch('http://localhost:3000/admin/signup',{method:'POST',
+              body:JSON.stringify({
+                username,
+                password
+              }),
+              headers: {
+                'Content-Type': 'application/json'
+              }
+            }).then(callback1)
+            }}>
+              Sign up
+                </Button>
+        </Card>
+        </center>
+      </div>
+    )
+  }
+  
+ 
