cf4e0cce3d29076dd06dbbbf3c58c9b77e922ff5
Fatima Irshad <154042550+FatimaIrshad123@users.noreply.github.com>
2024-01-27 10:56:18 +0500

    Update admin.js

--- Backened/middleware/admin.js
+++ Backened/middleware/admin.js
@@ -1,14 +1,11 @@
 const { Admin } = require("../db");
 const { jwtSecret } = require('../config')
-// Middleware for handling auth
 var jwt = require('jsonwebtoken');
 
 function adminMiddleware(req, res, next) {
-    // Implement admin auth logic
 
     let token = req.headers.authorization;
-    console.log(token)
-    // let jwtToken = token.split(' ')[1];
+   
     if (token){
         jwt.verify(token,jwtSecret,(err,user)=>{
             if (err){
@@ -20,16 +17,6 @@ function adminMiddleware(req, res, next) {
     } else {
         res.sendStatus(401);
     }
-            }
-        
-    
-    // const decoded = jwt.verify(token,jwtSecret);
-    // console.log(decoded)
-    // if (decoded.username){
-    //     next()
-    // }else {
-    //     res.status(403).json({msg:'You are not Authenticated'})
-    // }
-// }
+}
 
 module.exports = adminMiddleware;
