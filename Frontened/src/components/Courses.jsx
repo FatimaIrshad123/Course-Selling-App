69b47ef4f4c243cb341c95e761c878edb6faa135
Fatima Irshad <154042550+FatimaIrshad123@users.noreply.github.com>
2024-01-28 11:45:01 +0500

    Update Courses.jsx

--- Frontened/src/components/Courses.jsx
+++ Frontened/src/components/Courses.jsx
@@ -10,16 +10,14 @@ export default function Courses(){
               setData(data[i].map(x => {return (
                 <div key={x._id} >
                   <Card>
-                  <Typography variant={"h4"}> {x.title}</Typography>
-                  <Typography variant={"h5"}> {x.description}</Typography>
-                  <Typography variant={"h6"}> {x.price}</Typography>
-                  <br></br>
+                      <Typography variant={"h4"}> {x.title}</Typography>
+                      <Typography variant={"h5"}> {x.description}</Typography>
+                      <Typography variant={"h6"}> {x.price}</Typography>
+                      <br></br>
                   </Card>
                 </div>
                 )}))
-            }
-          }
-
+            }}
           function callback1(res){
             res.json().then(callback2)
           }
@@ -27,13 +25,11 @@ export default function Courses(){
             headers:{'Authorization' : localStorage.getItem('token')}
         }).then(callback1)
     },[])
-
     return(
- <center>   
-        <div style = {{border: '2px',width:400,padding:20, marginTop:30, backgroundColor:'white'}}>
-          {data}
-        </div>
-      
-        </center>  
+         <center>   
+                <div style = {{border: '2px',width:400,padding:20, marginTop:30, backgroundColor:'white'}}>
+                      {data}
+                </div>
+        </center>
     )
 }
