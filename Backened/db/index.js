a96e2421104ffd75ff6645a5b418e393acd536e4
Fatima Irshad <154042550+FatimaIrshad123@users.noreply.github.com>
2024-04-20 21:32:53 +0500

    Update index.js

--- Backened/db/index.js
+++ Backened/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('your mongodb url');
+mongoose.connect('mongodb+srv://Fatima:Fatima@cluster0.7bunznc.mongodb.net/');
 
 const AdminSchema = mongoose.Schema({
     username : String,
